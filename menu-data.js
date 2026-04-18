892d7b62b146e8090f96d861714f9a4bc696bc0f
Thayguara <thaygauarabatista34@gmail.com>
2026-04-18 03:57:51 -0300

    Codigo simplificado

--- menu-data.js
+++ menu-data.js
@@ -1,34 +1,34 @@
 const menuItems = [
     {
-        id: 7,
+        id: 1,
         name: "Brasa Vini",
-        description: "Pão brioche, queijo cheddar ou mussarela (opção do cliente), blend (120g), bacon, cebola caramelizada, geleia de pimenta, molho grill e cebola empanada",
+        description: "Pão brioche, queijo, blend (120g), bacon, cebola caramelizada e geleia de pimenta.",
         price: 25.00,
-        image: "./assets/imagens/brasa-vini.jpg",
+        image: "assets/imagens/brasa-vini.jpg",
         category: "Burguers"
     },
     {
         id: 2,
         name: "Super Brasa",
-        description: "2 queijo cheddar ou mussarela (opção do cliente), 02 blends (120g cada) e bacon (fatiado)",
+        description: "2 queijos, 2 blends (120g cada) e bacon fatiado.",
         price: 24.00,
-        image: "./assets/imagens/superbrasa.jpg",
+        image: "assets/imagens/superbrasa.jpg",
         category: "Burguers"
     },
     {
-        id: 6,
+        id: 3,
         name: "Brasa Gorgonzola",
-        description: "Pão brioche, queijo cheddar ou mussarela (opção do cliente), blend (120g), bacon, cebola caramelizada e gorgonzola",
+        description: "Pão brioche, blend (120g), bacon, cebola caramelizada e gorgonzola.",
         price: 21.00,
-        image: "./assets/imagens/brasagorgonzola.jpg",
+        image: "assets/imagens/brasagorgonzola.jpg",
         category: "Burguers"
     },
     {
-        id: 8,
+        id: 4,
         name: "Coca-Cola 1L",
-        description: "Refrigerante Coca-Cola garrafa de 1 litro gelada",
+        description: "Refrigerante gelado 1 litro.",
         price: 9.00,
-        image: "./assets/imagens/coca1litro.png",
+        image: "assets/imagens/coca1litro.png",
         category: "Bebidas"
     }
 ];
